95af8bf8839520667c34f4880fc3b8fa323114b9
ajoscram <ajoscram@gmail.com>
2020-01-12 19:36:33 -0600

    Added the proper route, icons and information needed.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-markerOptions = {
-    riseOnHover: true
-}
+const start = L.icon({
+    iconUrl: 'assets/start.svg',
+    iconSize: [35, 35],
+    iconAnchor: [17.5, 17.5],
+    popupAnchor: [0, -17.5]
+});
+
+const finish = L.icon({
+    iconUrl: 'assets/finish.svg',
+    iconSize: [35, 35],
+    iconAnchor: [17.5, 17.5],
+    popupAnchor: [0, -17.5]
+});
+
+const burger = L.icon({
+    iconUrl: 'assets/burger.svg',
+    iconSize: [30, 30],
+    iconAnchor: [15, 15],
+    popupAnchor: [0, -15]
+});
 
 async function getPoints(){
     const response = await fetch("points.json");
@@ -9,19 +26,37 @@ async function getPoints(){
 }
 
 function main(){
-    const map = L.map('map').setView([9.7095171, -84.2432125], 8);
-    const atribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+    const map = L.map('map');
+    const attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> | Map icons by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>';
     const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    const tiles = L.tileLayer(tileUrl, { atribution });
+    const tiles = L.tileLayer(tileUrl, { attribution });
     tiles.addTo(map);
 
     getPoints().then(json => {
         const points = json.points;
         const route = [];
         for(i = 0; i < points.length; i++){
-            let waypoint = {latLng:L.latLng(points[i].latitude, points[i].longitude), name:points[i].location};
+            let waypoint = {latLng:L.latLng(points[i].latitude, points[i].longitude), name:"<b>"+points[i].location+"</b>"};
             route.push(waypoint);
         }
-        L.Routing.control({waypoints:route}).addTo(map);
+        //options for the routing control
+        //the route is passed via the waypoints property
+        L.Routing.control({ 
+            addWaypoints: false,
+            draggableWaypoints: false,
+            waypoints: route,
+            //this property is a function that defines how the markers are rendered
+            createMarker: 
+                (i, waypoint, n) => {
+                    let icon = null;
+                    if(i == 0)
+                        icon = start;
+                    else if(i == n-1)
+                        icon = finish;
+                    else
+                        icon = burger;
+                    return L.marker(waypoint.latLng, {icon}).bindPopup(waypoint.name);
+                }
+        }).addTo(map);
     });
 }
